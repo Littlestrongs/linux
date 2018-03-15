f869270b02d8e5fa74e8a902119a733625d36116
ArnoldLu <arnoldlu@qq.com>
2018-03-15 09:40:35 +0800

    Add slub_debug test code.
    Signed-off-by: ArnoldLu <arnoldlu@qq.com>

---
+++ test_code/slub_debug/slub_test3.c
@@ -0,0 +1,33 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+
+static char *buf;
+
+static void create_slub_error(void)
+{
+  buf = kmalloc(32, GFP_KERNEL);
+  if(buf) {
+    kfree(buf);
+    printk("al: Access after free");
+    memset(buf, 0x55, 32);
+  }
+}
+
+static int __init my_test_init(void)
+{
+  printk("al: slub error test init\n");
+  create_slub_error();
+  return 0;
+}
+
+static int __exit my_test_exit(void)
+{
+  printk("al: slub error test exit\n");
+  return 0;
+}
+
+MODULE_LICENSE("GPL");
+module_init(my_test_init);
+module_exit(my_test_exit);
